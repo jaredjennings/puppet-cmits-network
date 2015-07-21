eb910979474138750f55a7884c0dcb674097279d
Jared Jennings <jared.jennings.ctr@us.af.mil>
2015-07-21 14:54:25 -0500

    fix quoting bugs in network::wifi::no::darwin

--- manifests/wifi/no/darwin.pp
+++ manifests/wifi/no/darwin.pp
@@ -27,12 +27,12 @@ class network::wifi::no::darwin {
 
     $nse = 'networkserviceenabled'
     exec { 'disable AirPort network service':
-        command => 'networksetup -set${nse} AirPort off',
-        onlyif => 'networksetup -get${nse} | grep Enabled',
+        command => "networksetup -set${nse} AirPort off",
+        onlyif => "networksetup -get${nse} | grep Enabled",
     }
     exec { 'disable Wi-Fi network service':
-        command => 'networksetup -set${nse} Wi-Fi off',
-        onlyif => 'networksetup -get${nse} | grep Enabled',
+        command => "networksetup -set${nse} Wi-Fi off",
+        onlyif => "networksetup -get${nse} | grep Enabled",
     }
 
 # \implements{macosxstig}{OSX00385 M6}%
@@ -43,34 +43,17 @@ class network::wifi::no::darwin {
 # possible for a network service to own multiple interfaces.
     exec { 'turn off AirPort power':
 # So---if any Wi-Fi or AirPort devices have power On...
-        onlyif => "\
-              networksetup -listnetworkserviceorder | \
+        onlyif => "networksetup -listnetworkserviceorder | \
               grep -A1 'Wi-Fi\\|AirPort' | \
               grep -o 'Device: [a-z0-9]\\+' | \
               cut -d: -f2 | \
               xargs -n 1 networksetup -getairportpower | \
               grep 'On\$'",
 # ...turn off power to all Wi-Fi or AirPort devices.
-        command => "\
-              networksetup -listnetworkserviceorder | \
+        command => "networksetup -listnetworkserviceorder | \
               grep -A1 'Wi-Fi\\|AirPort' | \
               grep -o 'Device: [a-z0-9]\\+' | \
               cut -d: -f2 | \
               xargs -I % networksetup -setairportpower % Off",
     }
-
-This is done using System Preferences. Open the Network section;
-for each active AirPort interface in the pane on the left, click the
-interface, and click ``Turn AirPort Off.'' After all of this, click
-``Apply.''
-
-This is done using System Preferences.
-\doneby{admins}{macosxstig}{OSX00400 M6}%
-Turn off IPv6 on Macs ``if not being used.''
-
-This is done using System Preferences. Open the Network section;
-for each active interface in the pane on the left, click the interface,
-click the ``Advanced...'' button toward the lower right, and in the TCP/IP
-tab, change the ``Configure IPv6'' setting to ``Off.'' After all of this,
-click ``Apply.''
 }
